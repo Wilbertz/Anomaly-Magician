944c5fe6aea9a8fbe8d975c2c7d8eb851bea62fd
Harald Wilbertz <harald.wilbertz@gmx.de>
2025-08-16 19:29:19 +0200

    Code to create sample data.

--- schemas/schema.py
+++ schemas/schema.py
@@ -1,16 +1,19 @@
 from sqlalchemy import create_engine
 from sqlmodel import Field, SQLModel
 
-class Hero(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    name: str
-
-connection_string = (
+CONNECTION_STRING = (
     "mssql+pyodbc://@DESKTOP-2LMIUA2/Anomaly-Magician"
     "?driver=ODBC+Driver+17+for+SQL+Server"
     "&trusted_connection=yes"
 )
-engine = create_engine(connection_string)
 
-with engine.connect() as conn:
-    pass
+class SampleTable(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    value: str = Field(nullable=True, max_length=255)
+
+def create_sample_database(connection_string=CONNECTION_STRING):
+    engine = create_engine(connection_string, echo=True)
+    SQLModel.metadata.create_all(engine)
+
+if __name__ == "__main__":
+    create_sample_database()
