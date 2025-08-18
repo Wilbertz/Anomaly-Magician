e56a0c1af341e407b6b15da4c6fc4c8782231699
Harald Wilbertz <harald.wilbertz@gmx.de>
2025-08-18 19:09:59 +0200

    Additional test added.

---
+++ configuration/config.py
@@ -0,0 +1,11 @@
+class Config():
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super(Config, cls).__new__(cls)
+        return cls._instance
+
+    def __init__(self):
+        self.config = {}
+
--- tests/test_configuration.py
+++ tests/test_configuration.py
@@ -1,2 +1,9 @@
+from configuration.config import Config
+
+
 def test_dummy():
     assert 1 == 1
+
+def test_config_singleton():
+    config = Config()
+    assert config
