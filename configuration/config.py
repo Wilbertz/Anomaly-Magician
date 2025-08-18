cde6e69612a8a5ab91ceecf06143d045399cb76b
Harald Wilbertz <harald.wilbertz@gmx.de>
2025-08-18 19:45:19 +0200

    Tests improved with monkeypatch.

--- configuration/config.py
+++ configuration/config.py
@@ -1,11 +1,35 @@
-class Config():
+import json
+
+
+def _read_config() -> dict:
+    with open(".\configuration\config.json") as f:
+        _config = json.load(f)
+    return _config
+
+
+class Config:
+    """
+    This class (implemented as a singleton) implements access to application configuration.
+    """
     _instance = None
+    _config: dict = None
+    _server: str = None
+    _database: str = None
+    _table: str = None
+    _column: str = None
 
     def __new__(cls, *args, **kwargs):
         if not cls._instance:
             cls._instance = super(Config, cls).__new__(cls)
         return cls._instance
 
-    def __init__(self):
-        self.config = {}
+    def __init__(self) -> None:
+        try:
+            self._config = _read_config()
+            self._server = self._config["database"]["server"]
+            print(self._server)
+        except FileNotFoundError:
+            raise FileNotFoundError()
+
+
 
--- tests/test_configuration.py
+++ tests/test_configuration.py
@@ -1,9 +1,21 @@
+import os
+from pathlib import Path
+import pytest
 from configuration.config import Config
 
+@pytest.fixture(autouse=True)
+def run_before_tests(monkeypatch):
+    monkeypatch.chdir(Path(os.getcwd()).parent)
 
 def test_dummy():
     assert 1 == 1
 
 def test_config_singleton():
+    config1 = Config()
+    assert config1
+    config2 = Config()
+    assert config1 == config2
+
+def test_config_configuration():
     config = Config()
-    assert config
+    assert not config._server == None
