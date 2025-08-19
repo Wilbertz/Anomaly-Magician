5bd209876d3bd9161e2f3906478405b4b79ed7d9
Harald Wilbertz <harald.wilbertz@gmx.de>
2025-08-19 19:58:24 +0200

    Additional methods added to code base class.

--- codes/CodeModel.py
+++ codes/CodeModel.py
@@ -4,4 +4,25 @@ from pydantic import BaseModel
 class CodeModel(BaseModel):
     name: str
     industry: str | None
-    iso_code: Bool
+    iso_code: bool
+    min_length: int
+    max_length: int
+    fixed_length: int | None
+    regex: str | None
+    values: list[str] | None
+
+    def simple_check(self)  -> bool:
+        """
+        A quick and fast way to check if the code is syntactically correct.
+        No check is attempted to check whether an instance of the object
+        identified by this code really exists.
+        """
+        pass
+
+    def complex_check(self) -> bool:
+        """
+        A time-consuming and potentially expansive way to check both whether
+        the code is syntactically correct and whether an instance of the object
+        really exists in the real world. Typically, an external service is called.
+        """
+        pass
