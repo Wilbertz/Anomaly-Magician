52a959e1f1421327c64fc610c273bfe1baf92d24
Harald Wilbertz <harald.wilbertz@gmx.de>
2025-08-19 19:42:57 +0200

    Comment added.

--- codes/VinCode.py
+++ codes/VinCode.py
@@ -2,4 +2,9 @@ from codes.CodeModel import CodeModel
 
 
 class VinCode(CodeModel):
+    """
+    A Vehicle Identification Number (VIN) is used to uniquely
+    identify automobiles and other vehicles. The VIN was originally
+    described in ISO-3779 in 1977 and last revised in 1983.
+    """
     name: str
