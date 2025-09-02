c83ccd475af98da1184fff6d7298c8e352279758
Harald Wilbertz <harald.wilbertz@gmx.de>
2025-09-02 16:34:07 +0200

    Work in progress.

--- schemas/schema.py
+++ schemas/schema.py
@@ -1,10 +1,11 @@
 from sqlmodel import Field, SQLModel, Session
 
 class VinCodesTable(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    vin_fixed_length_valid_regex: str = Field(nullable=True, max_length=255)
-    vin_fixed_length_invalid_regex: str = Field(nullable=True, max_length=255)
-    vin_variable_length: str = Field(nullable=True, max_length=255)
+    id: int | None = Field(default=None, primary_key=True, description="The primary key column.")
+    vin_valid: str = Field(nullable=True, max_length=255, description="Syntactically valid VIN codes.")
+    vin_valid_length_invalid_regex: str = Field(nullable=True, max_length=255, description="VIN with valid length but invalid regex.")
+    vin_invalid_length: str = Field(nullable=True, max_length=255, description="VIN with invalid length.")
+    name: str = Field(nullable=True, max_length=255, description="An arbitrary name.")
 
 if __name__ == "__main__":
    pass
